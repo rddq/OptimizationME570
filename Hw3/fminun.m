5d81a9743a288d85f9f02b22a64c0dd06499f3a8
Ryan Day <rddays@gmail.com>
2019-02-06 12:01:39 -0700

    refactored code to use recursion, steepest descent works

--- Hw3/fminun.m
+++ Hw3/fminun.m
@@ -1,46 +1,63 @@
 function [xopt, fopt, exitflag] = fminun(obj, gradobj, x0, stoptol, algoflag)     
+               
         %--- Set constants ---%
         [n,~] = size(x0); 
         stoptol_vector = stoptol*ones(1,n);
-        alphaInitial = 0.000005;
+        alphaInitial = 0.00005;
         
         %--- Initialize variables ---%      
         f = obj(x0);
         alpha = alphaInitial;
-        grad = gradobj(x);
+        grad = gradobj(x0);
+        if abs(grad) < stoptol_vector
+            xopt = x0;
+            fopt = f;
+            exitflag = 0;
+            return
+        else
         if algoflag == 1
             s = getSdDirection(grad);
         else
             s = getQNdirection(grad);
-        end             
-        [a,fn] = checkAlpha(x0,f,obj,alpha,0,0);       
-        alpha = quadraticFit(a,fn);
+        end 
+        
+        %--- Do a line search ---%
+        [a,fn] = lineSearch(x0,f,s,obj,alpha);
+        [a,fn] = takeBestThreePoints(a,fn);
+        alpha = quadraticFit(a,fn);   
         
         %--- Set new variables for new search direction ---%
-        xnew = x + alpha*s;
-        %fnew = obj(xnew);
-        grad = gradobj(x);
-        if abs(grad) < stoptol_vector
-            xopt = x;
-            fopt = f;
-            exitflag = 1;
-        else
-            fminun(obj, gradobj, xnew, stoptol, algoflag)
+        xnew = x0 + alpha*s;
+        [xopt, fopt, exitflag] = fminun(obj, gradobj, xnew, stoptol, algoflag);
         end     
     end
         
-    function [a,fn] = checkAlpha(x0,f,s,obj,alpha,ahistory,fhistory)
+    function [a,fn] = lineSearch(x0,f,s,obj,alpha)
         xnew = x0 + alpha*s;
         fnew = obj(xnew);
         if fnew < f
-           checkAlpha(x0,fnew,s,obj,alpha*2,[alpha,alpha*2],[f,fnew])
+           [a,fn] = lineSearchHelper(x0,fnew,s,obj,alpha*2,[0, alpha],[f,fnew]);
         else
-           [a1]
-           return 
+            error('second point started higher')
+        end
+        amiddle = a(end)*0.75;
+        fmiddle = obj(x0+amiddle*s);
+        a = [a((end-2):end-1),amiddle,a(end)];
+        fn = [fn((end-2):end-1),fmiddle,fn(end)];
+    end
+    
+    function [a,fn] = lineSearchHelper(x0,f,s,obj,alpha,ahistory,fhistory)
+        xnew = x0 + alpha*s;
+        fnew = obj(xnew);
+        if fnew < f
+           [a,fn] = lineSearchHelper(x0,fnew,s,obj,alpha*2,[ahistory alpha],[fhistory,fnew]);
+        else
+            a = [ahistory alpha];
+            fn = [fhistory fnew];
         end
     end
     
-    function [astar] = quadraticFit(a,fn)
+    function [astar] = quadraticFit(a,fn) 
         % Calculate Alpha of minimum of the quadratic approximation
         num = fn(1)*(a(2)^2-a(3)^2)+fn(2)*(a(3)^2-a(1)^2)+fn(3)*(a(1)^2-a(2)^2);
         den = 2*(fn(1)*(a(2)-a(3))+fn(2)*(a(3)-a(1))+fn(3)*(a(1)-a(2)));
@@ -50,17 +67,18 @@ function [xopt, fopt, exitflag] = fminun(obj, gradobj, x0, stoptol, algoflag)
    function [s] = getQNdirection(grad)
         s=0;
    end
+   
+   function [a,fn] = takeBestThreePoints(a,fn)
+        [~,min_index] = min(fn);
+        a = a([min_index-1 min_index min_index+1]);
+        fn = fn([min_index-1 min_index min_index+1]);
+   end
+   
      % get steepest descent search direction as a column vector
      function [s] = getSdDirection(grad) 
         mag = sqrt(grad'*grad);
         s = -grad/mag;
      end
-     
-     function [fnew, xnew] = takeStep(x, alpha, s ,obj)
-        xnew = x + alpha*s;
-        fnew = obj(xnew);
-     end
-
 
     
      
--- Hw3/fminunDrivHW.m
+++ Hw3/fminunDrivHW.m
@@ -5,13 +5,16 @@
     global nobj ngrad
     nobj = 0; % counter for objective evaluations
     ngrad = 0.; % counter for gradient evaluations
-    x0 = [10; 10; 10]; % starting point, set to be column vector
+    
+    
+    x0_1 = [10; 10; 10]; % starting point, set to be column vector
+    x0_2 = [-1.5; 1];
     algoflag = 1; % 1=steepest descent; 2=BFGS quasi-Newton
     stoptol = 1.e-3; % stopping tolerance, all gradient elements must be < stoptol  
     
     
     % ---------- call fminun----------------
-    [xopt, fopt, exitflag] = fminun(@obj1, @gradobj1, x0, stoptol, algoflag);
+    [xopt, fopt, exitflag] = fminun(@obj2, @gradobj2, x0_2, stoptol, algoflag);
    
     xopt
     fopt
@@ -49,7 +52,7 @@
         global ngrad
         %gradient for function 1
         grad(1,1) = 2*x(1) - 400*x(1)*(- x(1)^2 + x(2)) - 2;
-        grad(2,1) = - 200*x(1)^2 + 200*x(2)
+        grad(2,1) = - 200*x(1)^2 + 200*x(2);
         ngrad = ngrad + 1;
      end
      
