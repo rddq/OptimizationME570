eee4681e83cd7e216caabd2d35f6581c4dcb2409
Ryan Day <rddays@gmail.com>
2019-02-10 21:20:06 -0700

    fixed bug with x0_2

--- Hw3/fminunDrivHW.m
+++ Hw3/fminunDrivHW.m
@@ -7,13 +7,13 @@
     ngrad = 0.; % counter for gradient evaluations
     
     algoflag = 1; % 1=steepest descent; 2=BFGS quasi-Newton
-    problem = 1;
+    problem = 2;
     stoptol = 1.e-3; % stopping tolerance, all gradient elements must be < stoptol  
     
     x0_1_starting_points = [[10;10;10] [2;7;9] [100;100;100] [15;9;30]];
     x0_1 = x0_1_starting_points(:,1); % starting points, set to be column vector
     x0_2_starting_points = [[-1.5; 1] [-3; 1.8]];
-    x0_2 = x0_2_starting_points(1);
+    x0_2 = x0_2_starting_points(:,1);
     
     if problem == 1
         % quadratic function
