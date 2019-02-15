7e1c9337e9d81403a234bf100082324b239368aa
Ryan Day <ryan.david.day@gmail.com>
2019-02-15 15:04:01 -0700

    works

--- Project/OptimizeTruss.m
+++ Project/OptimizeTruss.m
@@ -1,10 +1,20 @@
 function [xopt, fopt, exitflag, output] = OptimizeTruss()
 
     % ------------Starting point and bounds------------
-    %var = theta 1,L1,L2,L6,L7,Area    %design variables
-    x0 = [60,1.5,1.5,1.5,1.5]; %starting point
-    ub = [75,3,3,3,3]; %upper bound
-    lb = [45,0.5,0.5,0.5,0.5]; %lower bound
+       
+    % Set mins and maxes
+    theta_min = 45;
+    theta_max = 85;
+    theta_init = 60;
+    % Truss member length min and max
+    mL_Min = 0.5;
+    mL_Max = 3;
+    mL_init = 1.5;
+    
+    %var = theta 1,L1,L2,L6,L7    %design variables
+    x0 = [theta_init,mL_init,mL_init,mL_init,mL_init]; %starting point
+    ub = [theta_max,mL_Max,mL_Max,mL_Max,mL_Max]; %upper bound
+    lb = [theta_min,mL_Min,mL_Min,mL_Min,mL_Min]; %lower bound
 
     % ------------Linear constraints------------
     A = [];
@@ -15,33 +25,30 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
     % ------------Objective and Non-linear Constraints------------
     function [f, c, ceq] = objcon(x)
         %---Design Variables---%   
-        
         % Angle in degrees
-        theta1 = x(1);     
-        
+        theta1 = x(1);             
         % Length in meters
+        % Height is constrained with L1
+        % Length is constrained with L2 and L6 L7
         L1 = x(2);
         L2 = x(3);
         L6 = x(4);
-        L7 = x(5);
-        
-        % area of cross section of beam (assuming square beam)
+        L7 = x(5);       
+             
+        %---Constants---%
+        % Area of cross section of beam
         Area = 0.15^2*pi;
         
-        %---Constants---%
-        
+        % Annealed 1018 steel
         % Yield strength - MPa
         Sy = 220;
         % Ultimate Tensile Strength - MPa
         Sut = 341;  
         % Density kg/m^3
         density = 7850;
-        % Factor of Safety
-        FOS = 1.5;
         
-        % Degrees that angle of Forces will be shifted
-        angle_s = 10;
-                
+        FactorOfSafety = 1.5;
+                        
         F1 = 10000; %N
         F2 = 15000; %N
         phiF1 = 90; %degrees
@@ -49,83 +56,39 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
         
         %---Design Functions---%
         
-        % Calculate other truss dimensions
-        L3 = lawCosinesLength(L1,L6,theta1);
-        theta2 = lawCosinesAngle(L6,L1,L3);
-        theta3 = lawCosinesAngle(L1,L6,L3);
+        % Other Truss Dimensions
+        [theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,L3,L4,L5] = calculateOtherTrussDimensions(L1,L2,L6,L7,theta1);              
         
-        L4 = sqrt((L1*cosd(theta1)-L6)^2+(L1*sind(theta1))^2);
-        theta4 = lawCosinesAngle(L4,L2,L3);
-        theta5 = lawCosinesAngle(L2,L3,L4);
-        theta6 = lawCosinesAngle(L3,L2,L4);
-        
-        theta8 = 180-theta3-theta5;
-        L5 = lawCosinesLength(L4,L7,theta8);                
-        theta7 = lawCosinesAngle(L7,L4,L5);
-        theta9 = lawCosinesAngle(L4,L7,L5);
-        
-        all_member_stress = zeros(1,63);
-        
-        % Find Member Stress in all configurations
-        phiF1 = [phiF1-angle_s,phiF1,phiF1+angle_s];
-        phiF2 = [phiF2-angle_s,phiF2,phiF2+angle_s];
-        iteration = 0;
-        for index_phiF1 = 1:3
-            for index_phiF2 = 1:3
-                iteration = iteration+1;
-                memberForces = calculateForces(F1,phiF1(index_phiF1),F2,phiF2(index_phiF2),theta1,theta3,theta6,theta8,theta9,L1,L6,L5);
-                all_member_stress(7*iteration-6:7*iteration) = (memberForces./Area);
-            end
-        end
-              
-        function memberForces = calculateForces(F1,phiF1,F2,phiF2,theta1,theta3,theta6,theta8,theta9,L1,L6,L5)
-            % Reaction Forces
-            Rx = -(F1*cosd(phiF1)+F2*cosd(phiF2));
-            Qy = F1*sind(phiF1)*L1*cosd(theta1)+F2*sind(phiF2)*(L6-L5*cosd(theta9));
-            Ry = F1+F2-Qy;
-            
-            % Solve system of equations for member forces
-            Ax = [-cosd(theta1), 1, cosd(theta3), 0, 0, 0, 0 ];
-            Ay = [sind(theta1) 0 sind(theta3) 0 0 0 0 ];
-            Bx = [0 -1 0 -cosd(theta6) cosd(theta9) 0 0 ];
-            By = [0 0 0 -sind(theta6) -sind(theta9) 0 0 ];
-            Cx = [cosd(theta1) 0 0 0 0 1 0 ];
-            Cy = [sind(theta1) 0 0 0 0 0 0 ];
-            Dx = [0 0 -cosd(theta3) cosd(theta8) 0 -1 1];
-            Dy = [0 0 sind(theta3) sind(theta8) 0 0 0];
-            Ex = [0 0 0 0 -cosd(theta9) 0 -1];
-            Ey = [0 0 0 0 sind(theta9) 0 0];
-            
-            A_mat = [Ax;Ay;Bx;By;Cx;Cy;Dx;Dy;Ex;Ey];
-            b_mat = [-F1*cosd(phiF1); F1*sind(phiF1); -F2*cosd(phiF2); F2*sind(phiF2); -Rx; -Ry; 0; 0; 0; -Qy];
-            memberForces = mldivide(A_mat,b_mat);
-        end
-
+        % Find Member Stresses
+        memberForces = calculateForces(F1,phiF1,F2,phiF2,theta1,theta3,theta6,theta8,theta9,L1,L6,L5);
+        all_member_stress = (memberForces./Area);
+             
         %---Objective function---%
         weight = (L1+L2+L3+L4+L5+L6+L7)*Area*density;
-        f = weight; %minimize weight
+        f = weight; % minimize weight
                 
         %---Inequality constraints (c<=0)---%
-        [constraint_number,~] = size(all_member_stress);
-        c = zeros(constraint_number*2,1); % create column vector
-        for index = 1:size(all_member_stress)
-            c(2*index-1) = all_member_stress(index)-Sy/(FOS);
-            c(2*index) = -Sut/(FOS)-all_member_stress(index);
+        
+        % Create constraints for members        
+        % initialize column vector 
+        [numberOfMembers,~] = size(all_member_stress);
+        c = zeros(numberOfMembers*2,1); 
+        
+        maxCompressiveForce = Sy/(FactorOfSafety);
+        maxTensionForce = Sut/(FactorOfSafety);
+        % Create constraint for each member
+        for index = 1:size(numberOfMembers)
+            c(2*index-1) = all_member_stress(index)-maxCompressiveForce;
+            c(2*index) = -Sut/maxTensionForce-all_member_stress(index);
         end
-        theta = [theta1,theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9];
-        c(64) = 35-theta2;
-        c(65) = 35-theta3;
-        c(66) = 35-theta5;
-        c(67) = 35-theta6;
-        c(68) = 35-theta7;
-        c(69) = 35-theta9;
-        ul = 85;
-        c(70) = theta2 - ul;
-        c(71) = theta3 - ul;
-        c(72) = theta5 - ul;
-        c(73) = theta6 - ul;
-        c(74) = theta7 - ul;
-        c(75) = theta9 - ul;
+        
+        % Create min and max constraint for each theta
+        thetas = [theta2,theta3,theta5,theta6,theta7,theta9];
+        offset = 2*numberOfMembers;
+        for index = 1:6
+            c(2*index+offset-1) = theta_min - thetas(index);
+            c(2*index+offset) = thetas(index) - theta_max;
+        end 
         % Height >1, <2.5
         c(76) = 1 - L1*(sind(theta1));
         c(77) = L1*(sind(theta1)) - 2.5;
@@ -137,7 +100,7 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
     end
 
     % ------------Call fmincon------------
-    options = optimoptions(@fmincon,'display','iter-detailed','Diagnostics','on','MaxFunctionEvaluations',50000,'OptimalityTolerance',1e-6);
+    options = optimoptions(@fmincon,'display','iter-detailed','Diagnostics','on');
     [xopt, fopt, exitflag, output] = fmincon(@obj, x0, A, b, Aeq, beq, lb, ub, @con, options);
     xopt %design variables at the minimum
     fopt %objective function value at the minumum  fopt = f(xopt)
@@ -164,21 +127,10 @@ function plot_truss(xopt)
     L2 = xopt(3);
     L6 = xopt(4);
     L7 = xopt(5);
-    % Calculate other truss dimensions
-    L3 = lawCosinesLength(L1,L6,theta1);
-    theta2 = lawCosinesAngle(L6,L1,L3);
-    theta3 = lawCosinesAngle(L1,L6,L3);
-
-    L4 = sqrt((L1*cosd(theta1)-L6)^2+(L1*sind(theta1))^2);
-    theta4 = lawCosinesAngle(L4,L2,L3);
-    theta5 = lawCosinesAngle(L2,L3,L4);
-    theta6 = lawCosinesAngle(L3,L2,L4);
-
-    theta8 = 180-theta3-theta5;
-    L5 = lawCosinesLength(L4,L7,theta8);                
-    theta7 = lawCosinesAngle(L7,L4,L5);
-    theta9 = lawCosinesAngle(L4,L7,L5);
- 
+    
+    [theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,L3,L4,L5]...
+        = calculateOtherTrussDimensions(L1,L2,L6,L7,theta1);
+    
     C = [0,0];
     A = [L1*cosd(theta1),L1*sind(theta1)];
     B = [A(1)+L2,A(2)];
@@ -194,4 +146,44 @@ function plot_truss(xopt)
     hold on
     plot([A(1),D(1),B(1)],[A(2),D(2),B(2)])
 end
-        
+function [theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,L3,L4,L5] = calculateOtherTrussDimensions(L1,L2,L6,L7,theta1)
+    % Calculate other truss dimensions
+    L3 = lawCosinesLength(L1,L6,theta1);
+    theta2 = lawCosinesAngle(L6,L1,L3);
+    theta3 = lawCosinesAngle(L1,L6,L3);
+
+    % Calculation for L4 guarantees that L2 is vertical
+    L4 = sqrt((L1*cosd(theta1)-L6)^2+(L1*sind(theta1))^2);
+    theta4 = lawCosinesAngle(L4,L2,L3);
+    theta5 = lawCosinesAngle(L2,L3,L4);
+    theta6 = lawCosinesAngle(L3,L2,L4);
+
+    theta8 = 180-theta3-theta5;
+    L5 = lawCosinesLength(L4,L7,theta8);                
+    theta7 = lawCosinesAngle(L7,L4,L5);
+    theta9 = lawCosinesAngle(L4,L7,L5);
+end
+function memberForces = calculateForces(F1,phiF1,F2,phiF2,theta1,theta3,theta6,theta8,theta9,L1,L6,L5)
+    % Reaction Forces
+    Rx = -(F1*cosd(phiF1)+F2*cosd(phiF2));
+    Qy = F1*sind(phiF1)*L1*cosd(theta1)+F2*sind(phiF2)*(L6-L5*cosd(theta9));
+    Ry = F1+F2-Qy;
+
+    % Solve system of equations for member forces
+    Ax = [-cosd(theta1), 1, cosd(theta3), 0, 0, 0, 0 ];
+    Ay = [sind(theta1) 0 sind(theta3) 0 0 0 0 ];
+    Bx = [0 -1 0 -cosd(theta6) cosd(theta9) 0 0 ];
+    By = [0 0 0 -sind(theta6) -sind(theta9) 0 0 ];
+    Cx = [cosd(theta1) 0 0 0 0 1 0 ];
+    Cy = [sind(theta1) 0 0 0 0 0 0 ];
+    Dx = [0 0 -cosd(theta3) cosd(theta8) 0 -1 1];
+    Dy = [0 0 sind(theta3) sind(theta8) 0 0 0];
+    Ex = [0 0 0 0 -cosd(theta9) 0 -1];
+    Ey = [0 0 0 0 sind(theta9) 0 0];
+    
+    % Ax = b
+    A_mat = [Ax;Ay;Bx;By;Cx;Cy;Dx;Dy;Ex;Ey];
+    b_mat = [-F1*cosd(phiF1); F1*sind(phiF1); -F2*cosd(phiF2); F2*sind(phiF2); -Rx; -Ry; 0; 0; 0; -Qy];
+    memberForces = mldivide(A_mat,b_mat);
+end
+
