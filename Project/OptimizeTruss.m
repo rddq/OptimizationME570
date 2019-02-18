26483af47f4df01ca6bba73eadee8cdee22cedd8
Ryan Day <ryan.david.day@gmail.com>
2019-02-18 13:54:04 -0700

    renamed variables and changed buckling constraint to only apply to members under compression

--- Project/OptimizeTruss.m
+++ Project/OptimizeTruss.m
@@ -24,7 +24,7 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
     beq = [];
 
     % ------------Objective and Non-linear Constraints------------
-    function [f, c, ceq, memberForces] = objcon(x)
+    function [f, c, ceq, memberStresses] = objcon(x)
         %---Design Variables---%   
         % Angle in degrees
         theta1 = x(1);             
@@ -47,7 +47,7 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
         % Young's modulus - Pascal
         Esteel = 205e9;
       
-        FactorOfSafety = 1.5;
+        factorOfSafety = 1.5;
               
         F1 = 10000; %N
         F2 = 15000; %N
@@ -59,8 +59,7 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
         [theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,L3,L4,L5] = calculateOtherTrussDimensions(L1,L2,L6,L7,theta1);              
         
         % Find Member Stresses
-        memberForces = calculateForces(F1,phiF1,F2,phiF2,theta1,theta3,theta6,theta8,theta9,L1,L6,L5);
-        all_member_stress = (memberForces./Area);
+        memberStresses = calculateStresses(F1,phiF1,F2,phiF2,theta1,theta3,theta6,theta8,theta9,L1,L6,L5,Area);
              
         %---Objective function---%
         weight = (L1+L2+L3+L4+L5+L6+L7)*Area*density;
@@ -68,15 +67,15 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
                 
         %---Inequality constraints (c<=0)---%
         % initialize constraint column vector 
-        [numberOfMembers,~] = size(all_member_stress);
+        [numberOfMembers,~] = size(memberStresses);
         c = zeros(numberOfMembers*2+16+3,1); 
         
-        maxCompressiveForce = Sy/(FactorOfSafety);
-        maxTensionForce = -Sut/(FactorOfSafety);
+        maxCompressiveForce = Sy/(factorOfSafety);
+        maxTensionForce = -Sut/(factorOfSafety);
         % Create min and max constraint for each member
         for index = 1:numberOfMembers
-            c(2*index-1) = (all_member_stress(index)-maxCompressiveForce);
-            c(2*index) = maxTensionForce-all_member_stress(index);
+            c(2*index-1) = (memberStresses(index)-maxCompressiveForce);
+            c(2*index) = maxTensionForce-memberStresses(index);
         end 
         % Create min and max constraint for each theta
         thetas = [theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9];
@@ -85,22 +84,22 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
             c(2*index+offset-1) = theta_min - thetas(index);
             c(2*index+offset) = thetas(index) - theta_max;
         end     
-        % Truss Height > 1 m
         trussHeight = sind(theta1);
+        % Truss Height > 1 m
         c(31) = 0.5 - L1*trussHeight;
         % Truss Height < 2.5 m
         c(32) = L1*trussHeight - 2.5;
         % Bottom of the truss is bigger than top of the truss
         c(33) = L2-(L6+L7);
-        % Buckling Constraints
+        % Buckling Constraints - only apply if member is under compression
         I_square_beam = sqrt(Area)^4/12;
-        c(34) = all_member_stress(1) - f_buckle_crit(Esteel,I_square_beam,L1);
-        c(35) = all_member_stress(2) - f_buckle_crit(Esteel,I_square_beam,L2);
-        c(36) = all_member_stress(3) - f_buckle_crit(Esteel,I_square_beam,L3);
-        c(37) = all_member_stress(4) - f_buckle_crit(Esteel,I_square_beam,L4);
-        c(38) = all_member_stress(5) - f_buckle_crit(Esteel,I_square_beam,L5);
-        c(39) = all_member_stress(6) - f_buckle_crit(Esteel,I_square_beam,L6);
-        c(40) = all_member_stress(7) - f_buckle_crit(Esteel,I_square_beam,L7);
+        L = [L1 L2 L3 L4 L5 L6 L7];
+        for index = 1:numberOfMembers
+            if memberStresses(index) > 0
+                BucklingConstraint = memberStresses(index) - f_buckle_crit(Esteel,I_square_beam,L(index));
+                c = [c; BucklingConstraint];
+            end
+        end
         
         %equality constraints (ceq=0)
         ceq = [];
@@ -112,10 +111,10 @@ function [xopt, fopt, exitflag, output] = OptimizeTruss()
     % ------------Call fmincon------------
     options = optimoptions(@fmincon,'display','iter-detailed','Diagnostics','on','MaxFunctionEvaluations',500000,'MaxIterations',10000);
     [xopt, fopt, exitflag, output] = fmincon(@obj, x0, A, b, Aeq, beq, lb, ub, @con, options);
-    [f, c, ceq, memberForces] = objcon(xopt);
+    [f, c, ceq, memberStresses] = objcon(xopt);
     xopt %design variables at the minimum
     fopt %objective function value at the minumum  fopt = f(xopt)
-    Forces_In_Each_Member = memberForces'
+    Stresses_In_Each_Member = memberStresses'
     objcon(xopt);
     plot_truss(xopt);
 
@@ -178,7 +177,7 @@ function [theta2,theta3,theta4,theta5,theta6,theta7,theta8,theta9,L3,L4,L5] = ca
     theta8 = lawCosinesAngle(L5,L4,L7);
     theta9 = lawCosinesAngle(L4,L7,L5);
 end
-function memberForces = calculateForces(F1,phiF1,F2,phiF2,theta1,theta3,theta6,theta8,theta9,L1,L6,L5)
+function memberStresses = calculateStresses(F1,phiF1,F2,phiF2,theta1,theta3,theta6,theta8,theta9,L1,L6,L5,Area)
     % Reaction Forces
     Rx = -(F1*cosd(phiF1)+F2*cosd(phiF2));
     Qy = F1*sind(phiF1)*L1*cosd(theta1)+F2*sind(phiF2)*(L6-L5*cosd(theta9));
@@ -200,4 +199,6 @@ function memberForces = calculateForces(F1,phiF1,F2,phiF2,theta1,theta3,theta6,t
     A_mat = [Ax;Ay;Bx;By;Cx;Cy;Dx;Dy;Ex;Ey];
     b_mat = [-F1*cosd(phiF1); F1*sind(phiF1); -F2*cosd(phiF2); F2*sind(phiF2); -Rx; -Ry; 0; 0; 0; -Qy];
     memberForces = mldivide(A_mat,b_mat);
+    % Calculate stress with F/A
+    memberStresses = memberForces./Area;
 end
