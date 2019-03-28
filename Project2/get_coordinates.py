cd997b8b22b4a5c767f4662eec611900b7109d0b
Ryan Day <ryan.david.day@gmail.com>
2019-03-27 18:15:40 -0600

    made basic framework for distances between points in the US

--- Project2/get_coordinates.py
+++ Project2/get_coordinates.py
@@ -1,6 +1,34 @@
-from python-osrm-master.osrm import osrm
+import requests
+import json
+from collections import namedtuple
+import pandas as pd
+import webbrowser
 
-points = [(-33.45017046193167,-70.65281867980957),
-          (-33.45239047269638,-70.65300107002258),
-          (-33.453867464504555,-70.65277576446533)]
-result = osrm.match(points, step=False, overview="simplified")
+def _make_request_string(temple1, temple2):
+    (lat1, long1) = temple1
+    (lat2, long2) = temple2
+    request = 'http://router.project-osrm.org/route/v1/driving/' + str(long1) + ',' + str(lat1) + ';'+ str(long2) + ',' + str(lat2) + '?overview=false'
+    return request
+
+filename = 'Project2/temples.txt'
+temples = pd.read_csv(filename, delim_whitespace=True)
+lat_coords = temples['lat']
+long_coords = temples['long'] # long is negative in the US
+
+index = 0
+index1 = 1
+temple1 = (lat_coords[index], long_coords[index])
+temple2 = (lat_coords[index1], long_coords[index1])
+address = _make_request_string(temple1,temple2)
+
+r = requests.get(address)
+data = r.content
+data_dict = json.loads(data)
+route = data_dict['routes'][0]
+
+distance = route['distance'] # in meters
+time = route['duration'] # in seconds
+print(str(distance))
+print(str(time))
+
+webbrowser.open_new_tab('https://map.project-osrm.org/?z=9&center=39.013849%2C-78.689575&loc=38.904793%2C-77.055960&loc=38.922491%2C-77.036640&loc=38.904793%2C-79.055961&hl=en&alt=0')
