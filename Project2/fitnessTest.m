92f348e4a26277c06c59a1e7d84872224d3cd0d1
Ryan Day <rddays@gmail.com>
2019-04-06 16:57:59 -0600

    code works, needs to be tested

--- Project2/fitnessTest.m
+++ Project2/fitnessTest.m
@@ -1,4 +1,9 @@
 clear
+w = importdata('Temple.txt'); % Read in Datafile
+temple_name = string(w.textdata(:,1)); % Pull Temple Names
+temple_state = string(w.textdata(:,2)); % Pull Temple Names
+temple_number = w.data(:,1); % Pull Number/Coords
+
 sessionFileName = 'TempleSchedules/templeEndowmentSchedules.json';
 global sessions
 sessions = jsondecode(fileread(sessionFileName));
@@ -12,7 +17,8 @@ x = importdata('DistanceAndTimeBetweenTemples/timeBetweenLocations.txt');
 global travel_time
 travel_time = x(2:end,2:end);
 
-daysotw = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday','Saturday'];
+global daysotw
+daysotw = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
 
 generation = (1:1:72);
 generation = generation.';
@@ -21,8 +27,7 @@ fitness(generation)
 function [time] = fitnessOfPath(path)
     global sessions
     global travel_time
-    time = 0;
-    startDate = datetime(2019,4,16,5,0,0,'TimeZone','local',...
+    startDate = datetime(2019,4,16,7,40,0,'TimeZone','local',...
     'Format','d-MMM-y HH:mm:ss Z');
     date = startDate;
     dayForm = 'long';
@@ -33,42 +38,59 @@ function [time] = fitnessOfPath(path)
         
         % Attend a Session
         [daynumber,currDay] = weekday(date,dayForm);
-        schedule = sessions(j).(currDay);
-        schedule = string(schedule);       
-        if ~isempty(schedule)
-            foundSession = 0;
-            [~,numberSessionsToday] = size(schedule);
-            for index = 1:numberSessionsToday
-                session = schedule(index);
-                tsess = getSessionDateTime(session, j, date);
-                difference = tsess-date;
-                if minutes(difference) > 20
-                    date = date + difference + hours(2);
-                    foundSession = 1;
-                    break
+        if daynumber ~= 1
+            schedule = sessions(j).(currDay);
+            schedule = string(schedule);       
+            if ~isempty(schedule)          
+                foundSession = 0;
+                [~,numberSessionsToday] = size(schedule);
+                for index = 1:numberSessionsToday
+                    session = schedule(index);
+                    tsess = getSessionDateTime(session, j, date, 0);
+                    difference = tsess-date;
+                    if minutes(difference) > 20
+                        date = date + difference + hours(2);
+                        foundSession = 1;
+                        break
+                    end
+                end
+                if foundSession
+                    continue
                 end
             end
-            if foundSession
-                continue
-            end
-        end        
-        % If no available session, try the next day.
+        end
+        % If no available session, try the first session of the next day.
         sessionSearch = 1;
+        extraday = 0;
         while sessionSearch
-            daynumber = daynumber+1;
-            newDay = daysotw(daynumber)
+            extraday = extraday+1;
+            if daynumber < 7
+                daynumber = daynumber+1;
+            else
+                daynumber = 2;
+                extraday = extraday + 1;
+            end
+            global daysotw
+            newDay = daysotw(daynumber);
             schedule = sessions(j).(newDay);
-            schedule = string(schedule);
-            
+            schedule = string(schedule);           
             if isempty(schedule)
                 continue
-            end            
+            end
+            % Go to first session of the next day
+            tsess = getSessionDateTime(schedule(1), j, date, extraday);
+            difference = tsess-date;
+            if difference < 0
+                error("Session should not be before you get there")
+            end
+            date = date + difference + hours(2);
+            break
         end                 
-        time = 1;
     end
+    time = date-startDate;
 end
 
-function sesTime = getSessionDateTime(session, templeIndex, date)
+function sesTime = getSessionDateTime(session, templeIndex, date, extradays)
     global timezones
     info = split(session);
     time = split(info(1),':');
@@ -80,7 +102,10 @@ function sesTime = getSessionDateTime(session, templeIndex, date)
     Year = year(date);
     Month = month(date);
     Day = day(date);
-
+    if extradays ~= 0
+        Day = Day + extradays;
+    end
+    
     sesTime = datetime(Year,Month,Day,Hour,Minute,0,'TimeZone',timezones(templeIndex),...
             'Format','d-MMM-y HH:mm:ss Z');
 end
@@ -91,6 +116,7 @@ total_time = zeros(1,m);
 for i=1:m
     gen_time = 0;
     path = generation(:,i);
-    total_time(i) = fitnessOfPath(path)
+    total_time(i) = fitnessOfPath(path);
 end
+total_time
 end
